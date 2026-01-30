37d9cd66a69e7346efba73d78429a343db8432d5
VickySekhon <sekh4498@mylaurier.ca>
2026-01-29 22:32:50 -0500

    Reapply "fix: add execution time"
    This reverts commit b4d95ecd97bf66b31ade82a209465f0b5f3baedc.

--- A1/prime-gaps.sh
+++ A1/prime-gaps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=2 # Run on two nodes
 #SBATCH --ntasks-per-node=40 # 1 process per core (40 cores per node)
-#SBATCH --time=00:15:00 # My job will take at most 15 minutes to run, so find and reserve resources for 15 minutes
+#SBATCH --time=02:00:00 # My job will take at most 2 hours to run, so find and reserve resources for 2 hours
 #SBATCH --job-name=prime-gaps # Name of my job
 #SBATCH --output=prime-gaps%j.txt # Write the output to prime-gaps<jobID>.txt
 #SBATCH --mail-user=sekh4498@mylaurier.ca # Email me at this address if my job fails
