bc8f5709ce01ee1d02c4e1ef30a80a9cf8026351
VickySekhon <sekh4498@mylaurier.ca>
2026-01-29 20:22:34 -0500

    fix: change n to 1 million and tweak scheduler script

--- A1/debug-job.sh
+++ A1/debug-job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=40
-#SBATCH --time=00:15:00
-#SBATCH --job-name debug-job
-#SBATCH --output=debug-job-out%j.txt
+#SBATCH --nodes=2 # Run on two nodes
+#SBATCH --ntasks-per-node=40 # 1 process per core (40 cores per node)
+#SBATCH --time=00:15:00 # My job will take at most 15 minutes to run, so find and reserve resources for 15 minutes
+#SBATCH --job-name=prime-gaps # Name of my job
+#SBATCH --output=prime-gaps%j.txt # Write the output to prime-gaps<jobID>.txt
+#SBATCH --mail-user=sekh4498@mylaurier.ca # Email me at this address if my job fails
 #SBATCH --mail-type=FAIL
- 
 
-cd $SLURM_SUBMIT_DIR/A1
+cd $SLURM_SUBMIT_DIR/A1/output
 module restore ASSIGNMENT_1_MODULES
 
 mpirun -np 80 python3 ./prime-gaps.py
