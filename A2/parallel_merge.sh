8f48098a96d4686c6dba74b5df692878f4702fd7
VickySekhon <sekh4498@mylaurier.ca>
2026-02-15 18:55:26 -0500

    refactor: set job time to 24 hours, not sure how long it will take. Better to be safe.

--- A2/parallel_merge.sh
+++ A2/parallel_merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=2 # Run on two nodes
 #SBATCH --ntasks-per-node=40 # 1 process per core (40 cores per node)
-#SBATCH --time=05:00:00 # My job will take at most 5 hours to run, so find and reserve resources for 5 hours
+#SBATCH --time=24:00:00 # My job will take at most 5 hours to run, so find and reserve resources for 5 hours
 #SBATCH --job-name=parallel-merge # Name of my job
 #SBATCH --output=parallel-merge%j.txt # Write the output to parallel-merge<jobID>.txt
 #SBATCH --mail-user=sekh4498@mylaurier.ca # Email me at this address if my job fails
