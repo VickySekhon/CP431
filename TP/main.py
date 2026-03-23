6e7597661028248eb9e62cde2bec277078b3b977
VickySekhon <sekh4498@mylaurier.ca>
2026-03-22 20:39:15 -0400

    feat: change export type

--- TP/main.py
+++ TP/main.py
@@ -44,6 +44,7 @@ When you zoom: you are recomputing, not enlarging
 
 """
 
+
 class SaveFigure:
     def __init__(self, directory, file_name):
         self.directory = directory
@@ -59,14 +60,16 @@ class SaveFigure:
         plt.imshow(data, cmap="Reds")
         plt.colorbar(label="Intensity")
         plt.savefig(self.full_path)
-      
-    def create_pixel_csv(self, all_pixels: np.ndarray, c: complex, delimiter=",") -> None:
+
+    def create_pixel_csv(
+        self, all_pixels: np.ndarray, c: complex, delimiter=","
+    ) -> None:
         # Strip the brackets
         c = str(c)[1:-1]
-        np.savetxt(f"julia_{c}.csv", all_pixels, delimiter=delimiter)
+        np.savetxt(f"julia_{c}.npy", all_pixels, delimiter=delimiter)
+
 
 class Fractal:
-  
     CIRCLE_LOWER_BOUND = -2
     CIRCLE_UPPER_BOUND = 2
     ESCAPE_ITERATIONS = 50
@@ -175,10 +178,12 @@ def main():
 
             all_pixels[row_start:row_end, :] = subset
         # print(f"Computed fractal (pixel representation): {all_pixels}")
-        
-        figure = SaveFigure(os.path.join(os.curdir, "plots"), f"julia_{str(c)[1:-1]}.png")
+
+        figure = SaveFigure(
+            os.path.join(os.curdir, "plots"), f"julia_{str(c)[1:-1]}.png"
+        )
         figure.create_pixel_csv(all_pixels, c)
-        figure.create_heatmap(all_pixels)
+        # figure.create_heatmap(all_pixels)
 
 
 if __name__ == "__main__":
